31bea40291fdffab2fa0dd97a92c21ced04dfc3c
GrishaBony <gr_ichet@mail.ru>
2025-05-08 21:37:13 +0400

    set global prefix, configure CORS, cookieParser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: [process.env.NUXT_APP_URL, 'http://localhost:3000'],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
