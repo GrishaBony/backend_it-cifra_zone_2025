85eeed71a65f8de1fddfae6e05c95e237ce4545e
GrishaBony <gr_ichet@mail.ru>
2025-05-08 21:37:35 +0400

    upd dependencies, add .env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { TokenModule } from './token/token.module';
+import { OpenrouterModule } from './openrouter/openrouter.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    TokenModule,
+    OpenrouterModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
